b6be30becd85d55cd5b810e8f41cf5b8d4f8e777
Samir Shaikh <shaikhsamir4325@gmail.com>
2026-04-23 14:23:36 +0530

    Add files via upload

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import { courses as dummyCourses, type Course } from "@/data/courses";
 
-export const API_BASE = "http://127.0.0.1:8000/api";
+export const API_BASE = "https://lms-project-madm.onrender.com";
 
 // ================= HELPER =================
 

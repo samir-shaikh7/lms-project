cc9bd4ad08acb97eadce400d931d225cade29a04
Samir Shaikh <shaikhsamir4325@gmail.com>
2026-04-24 09:41:28 +0530

    Delete backend directory

--- backend/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import signup_view, login_view, profile_view
-
-urlpatterns = [
-    path('auth/signup/', signup_view),
-    path('auth/login/', login_view),
-    path('profile/', profile_view),
-]
